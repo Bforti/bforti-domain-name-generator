7726c98c39976806117a166dc21cf9eefb44c5af
Bforti <139017911+Bforti@users.noreply.github.com>
2023-09-14 00:22:35 +0000

    commit

--- app.js
+++ app.js
@@ -5,16 +5,29 @@ let adj=["best", "great","mostbeauty"];
 let sust=["siteweb", "hotdog","sport"];
 let exte=[".com", ".es",".ve"];
 
-for(let i=0;i<3;i++){
-    let a=i+1;
-    let b=i+2;
-    if(a==3 && b==4){
-        a=0;
-        b=1;}
-    if(b==3){
-    b=0;  }
+for(let pron of pronoun){
+    
+    for(let ad of adj){
         
-console.log(pronoun[i].concat("",adj[b],sust[a],exte[b]))
-console.log(pronoun[a].concat("",adj[i],sust[b],exte[i]))
-console.log(pronoun[b].concat("",adj[a],sust[i],exte[a]))}
+        for(let sus of sust){
+            for(let ex of exte){
+                console.log(pron+ad+sus+ex)
+            }
+        }
+    }
+
+}
+//for(let i=0;i<pronoun.length;i++){
+    
+//     let a=i+1;
+//     let b=i+2;
+//     if(a==3 && b==4){
+//         a=0;
+//         b=1;}
+//     if(b==3){
+//     b=0;  }
+        
+// console.log(pronoun[i].concat("",adj[b],sust[a],exte[b]))
+// console.log(pronoun[a].concat("",adj[i],sust[b],exte[i]))
+//console.log(pronoun[b].concat("",adj[a],sust[i],exte[a]))}
 
